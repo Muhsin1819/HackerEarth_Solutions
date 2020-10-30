329c1a2c6fb1ee4766a005a084b7de7272482e54
Muhsin <47053659+Muhsin1819@users.noreply.github.com>
2020-10-30 11:58:45 +0530

    split_houses.py

---
+++ split_houses.py
@@ -0,0 +1,12 @@
+def village_split(village, n):
+    if("HH" in village):
+        print("NO")
+    else:
+        print("YES")
+        print(village.replace(".", "B"))
+
+
+grid = int(input())
+village = input()
+n = len(village)
+village_split(village, n)
